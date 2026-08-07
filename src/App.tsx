29f5f3577a1267dc0807a75b092871ae1e46b87e
Mikita Pilets <mykytapilec@gmail.com>
2026-08-07 12:23:50 +0300

    feat: add quiz data model and sample questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { questions } from './data/questions';
+
 function App() {
   return (
     <main>
       <h1>Quiz App</h1>
+      <p>{questions.length} questions available</p>
     </main>
   );
 }
